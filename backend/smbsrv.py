9b68adcc504994d7e06029187b38dce10a8f01e2
uriart <loiro_8@hotmail.com>
2023-08-01 21:21:12 +0200

    master: Nuevo método eliminar pelicula + refactors

--- backend/smbsrv.py
+++ backend/smbsrv.py
@@ -1,52 +1,53 @@
-import os
-import re
 from fastapi import APIRouter
 from smb.SMBConnection import SMBConnection
-from dotenv import load_dotenv
-from trakt import Trakt
-
-load_dotenv()
 
 router = APIRouter()
 
-Trakt.configuration.defaults.client(
-    id=os.getenv('TRAKT_CLIENT_ID'),
-    secret=os.getenv('TRAKT_CLIENT_SECRET')
-)
+server = '192.168.0.16'
+username = ''
+password = ''
+share = 'media'
+directorio = 'pelis'
 
 @router.get("/list_files")
 def list_files():
     try:
-        # Configura la conexión con el servidor Samba
-        server = os.getenv('SAMBA_SERVER')
-        username = ''
-        password = ''
-        share = 'media'
-        directorio = 'pelis'
-
         conn = SMBConnection(username, password, 'python', 'samba')
         conn.connect(server)
         results = conn.listPath(share, directorio)
-        patron = r'(.+)\s\((\d{4})\)'
-
-        for obj in results:
-            print(obj.filename)
-            if obj.filename not in [".", ".."]:
-                coincidencia = re.match(patron, obj.filename)
-                if coincidencia:
-                    # Extraer el nombre y el año de las coincidencias
-                    peli = coincidencia.group(1)
-                    anio = coincidencia.group(2)
-                    trakt_return = Trakt['search'].query(
-                        media = 'movie',
-                        query = peli,
-                        year = anio,
-                        extended = 'full'
-                    )
-                    print(trakt_return)
-
-
         return [obj.filename for obj in results if obj.filename not in [".", ".."]]
-
     except Exception as e:
         return {"error": str(e)}
+    
+@router.delete("/delete_directory/{path}")
+def delete_directory(path):
+    try:
+        conn = SMBConnection(username, password, 'python', 'samba')
+        conn.connect(server)
+        delete_directory_recursive(conn, share, directorio + "/" + path)
+
+        return {"success": True}
+    except Exception as e:
+        return {"error": str(e)}
+
+def delete_directory_recursive(conn, share_name, directory_path):
+    # Obtener la lista de archivos y subdirectorios dentro del directorio
+    files = conn.listPath(share_name, directory_path)
+    # Eliminar archivos dentro del directorio
+    for f in files:
+        if f.filename not in ['.', '..']:
+            print(f.filename)
+            file_path = share_name + '/' + directory_path + '/' + f.filename
+            if f.isDirectory:
+                # Si es un subdirectorio, eliminar recursivamente
+                delete_directory_recursive(conn, file_path)
+            else:
+                # Si es un archivo, eliminarlo
+                try:
+                    conn.deleteFiles(share_name, file_path)
+                except Exception as e:
+                    print(e)
+                    return {"error": str(e)}
+    
+    # Eliminar el directorio en sí
+    conn.deleteDirectory(share_name, directory_path)
--- frontend/src/views/CurrentMovies.vue
+++ frontend/src/views/CurrentMovies.vue
@@ -1,49 +1,115 @@
 <template>
-    <div>
-      <div v-if="loading.value">Cargando...</div>
-      <div v-else class="m-5">
-        <div class="row row-cols-1 row-cols-md-4 g-4">
-            <!--div class="col" v-for="(item, index) in obj_final" :key="index">
-                <div class="card" >
-                    <img :src="item.image" class="card-img-top" :alt="item.title">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ item.title }}</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-            </div-->
+  <div>
+    <div v-if="loading.value">Cargando...</div>
+    <div v-else class="m-5">
+      <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="col" v-for="movie in movies_data" :key="movie.name">
+          <div class="card">
+            <img :src="movie.image" class="card-img-top" :alt="movie.name">
+            <div class="card-body">
+              <h5 class="card-title">{{ movie.name }}</h5>
+              <button @click="delete_movie(movie.name)" class="btn btn-danger">Eliminar</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <script lang="ts">
-  import { defineComponent, reactive, onMounted } from 'vue';
-  import axios, { Axios } from 'axios';
+  import { defineComponent, reactive, onMounted } from 'vue'
+  import axios from 'axios';
   
   export default defineComponent({
     setup() {
 
-      const loading = reactive<{ value: boolean }>({ value: true }); // Inicializar 'loading' como un objeto con propiedad 'value' booleana
-      
-      async function getListFiles() {
+      const loading = reactive<{ value: boolean }>({ value: true })
+      const imdb_uri = 'https://imdb-api.projects.thetuhin.com'
+      const backend_python = 'http://localhost:3001'
+      const movies_data = reactive<{ name: string; image: string; description: string }[]>([]);
+
+      onMounted(getMoviesTitles);
+
+      async function getMoviesTitles() {
         try {
-          // Realizar la solicitud HTTP GET al endpoint de la API utilizando axios
-          const movies = await axios.get<string[]>("http://localhost:3001/list_files");
-          loading.value = false; // Se ha terminado de cargar, establecemos loading a false
+          const response = await axios.get<string[]>(backend_python + "/list_files")
+          const names = response.data;
+          if (Array.isArray(names)) {
+            movies_data.push(...names.map((name) => ({ name, image: '', description: '' })));
+            loading.value = false;
+            await getMoviesData();
+          }
         } catch (error) {
-          console.error("Error al obtener la lista de directorios:", error);
+          console.error("Error al obtener la lista de directorios:", error)
+        }
+      }
+
+      async function getMoviesData() {
+        try {
+          for (let movie of movies_data) {
+            const response = await axios.get<string[]>(imdb_uri + '/search?query=' + movie.name);
+            const results = response.data.results;
+            if (results.length > 0) {
+              movie.image = results[0].image;
+              movie.description = results[0].imdb;
+            }
+          }
+        } catch (error) {
+          console.error("Error al obtener los datos de las películas:", error)
         }
       }
   
-      // Llamar getListFiles() al cargar la página
-      onMounted(getListFiles);
+      async function delete_movie(name: string) {
+        try {
+          console.log("Borrando la película:", name);
+          await axios.delete(backend_python + '/delete_directory/' + name);
+          const index = movies_data.findIndex(movie => movie.name === name);
+          if (index !== -1) {
+            movies_data.splice(index, 1);
+          }
+        } catch (error) {
+          console.error("Error al borrar la película:", error)
+        }
+      }
+
   
       // Devolvemos las variables que deseamos exponer desde el setup()
       return {
+        movies_data,
         loading,
-      };
-    },
-  });
-  </script>
+        delete_movie,
+      }
+    }
+  })
+</script>
   
+<style>
+  .card-img {
+    width: 400px;
+    height: 600px;
+  }
+</style>
